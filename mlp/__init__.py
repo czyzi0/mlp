2d9b6706cae3a88596c66bbd9461e175ced4eb95
czyzi0 <czyznikiewicz.mateusz@gmail.com>
2018-06-03 22:24:08 +0200

    Add module with activation functions

---
+++ mlp/__init__.py
@@ -0,0 +1,10 @@
+"""Package with definitions for multilayer perceptron estimator.
+
+"""
+
+from . import activation
+from . import metrics
+from . import multilayer_perceptron
+from . import utils
+
+from .multilayer_perceptron import MultilayerPerceptron
---
+++ mlp/activation.py
@@ -0,0 +1,104 @@
+"""Module with activation function definitions.
+
+"""
+
+import numpy as np
+
+
+def _identity(x: np.ndarray, derivative: bool = False) -> np.ndarray:
+    """Identity activation function.
+
+    Args:
+        x: Input for function.
+        derivative: Flag for calculating function derivative.
+
+    Returns:
+        Result.
+
+    """
+    if derivative:
+        return np.ones_like(x)
+    return x
+_identity._name = 'identity'
+
+
+def _relu(x: np.ndarray, derivative: bool = False) -> np.ndarray:
+    """ReLu activation function.
+
+    Args:
+        x: Input for function.
+        derivative: Flag for calculating function derivative.
+
+    Returns:
+        Result.
+
+    """
+    # pylint: disable=no-member
+    if derivative:
+        return np.heaviside(x, 0)
+    return np.maximum(x, 0)
+_relu._name = 'relu'
+
+
+def _sigmoid(x: np.ndarray, derivative: bool = False) -> np.ndarray:
+    """Sigmoid activation function.
+
+    Args:
+        x: Input for function.
+        derivative: Flag for calculating function derivative.
+
+    Returns:
+        Result.
+
+    """
+    x = np.maximum(x, -100.0)
+    sigmoid = 1 / (1 + np.exp(-x))
+    if derivative:
+        return sigmoid * (1 - sigmoid)
+    return sigmoid
+_sigmoid._name = 'sigmoid'
+
+
+def _softplus(x: np.ndarray, derivative: bool = False) -> np.ndarray:
+    """Softplus activation function.
+
+    Args:
+        x: Input for function.
+        derivative: Flag for calculating function derivative.
+
+    Returns:
+        Result.
+
+    """
+    x = np.maximum(x, -100.0)
+    if derivative:
+        return 1 / (1 + np.exp(-x))
+    return x + np.log(1 + np.exp(-x))
+_softplus._name = 'softplus'
+
+
+def _tanh(x: np.ndarray, derivative: bool = False) -> np.ndarray:
+    """Hyperbolic tangent activation function.
+
+    Args:
+        x: Input for function.
+        derivative: Flag for calculating function derivative.
+
+    Returns:
+        Result.
+
+    """
+    tanh = np.tanh(x)
+    if derivative:
+        return 1 - tanh * tanh
+    return tanh
+_tanh._name = 'tanh'
+
+
+_ACTIVATION_DICT = {
+    _identity._name: _identity,
+    _relu._name: _relu,
+    _sigmoid._name: _sigmoid,
+    _softplus._name: _softplus,
+    _tanh._name: _tanh,
+}
