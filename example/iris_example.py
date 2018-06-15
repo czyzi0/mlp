750e47d785005a20a4f86afbe68548aa343bb3c8
czyzi0 <czyznikiewicz.mateusz@gmail.com>
2018-06-15 14:07:50 +0200

    Finish Iris example. Add Iris example model

--- example/iris_example.py
+++ example/iris_example.py
@@ -1,11 +1,12 @@
-"""
+"""Classification example using Iris data set.
 
 """
 
 import argparse
+import csv
 import pathlib
 import sys
-from typing import List
+from typing import List, Tuple
 
 import numpy as np
 import requests
@@ -16,14 +17,26 @@ import mlp
 IRIS_URL = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 IRIS_FILE_PATH = pathlib.Path(__file__).parent / 'data' / 'iris.data'
 
+NAME2VEC = {
+    'Iris-setosa': [1.0, 0.0, 0.0],
+    'Iris-versicolor': [0.0, 1.0, 0.0],
+    'Iris-virginica': [0.0, 0.0, 1.0],
+}
+
 
 def parse_args(args: List[str]) -> argparse.Namespace:
-    """
+    """Parse arguments.
+
+    Args:
+        args: List of arguments given to script.
+
+    Returns:
+        Parsed arguments.
 
     """
     parser = argparse.ArgumentParser(description='Train new model or test existing model on Iris data.')
 
-    group = parser.add_mutually_exclusive_group()
+    group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-i', '--input-model-path', type=pathlib.Path, help='path to the model to be tested')
     group.add_argument('-o', '--output-model-path', type=pathlib.Path, help='path to file to save trained model in')
 
@@ -31,7 +44,10 @@ def parse_args(args: List[str]) -> argparse.Namespace:
 
 
 def download_iris(file_path: pathlib.Path) -> None:
-    """
+    """Download Iris data.
+
+    Args:
+        file_path: Path to file in which data will be saved.
 
     """
     response = requests.get(IRIS_URL, stream=True)
@@ -40,24 +56,92 @@ def download_iris(file_path: pathlib.Path) -> None:
             iris_file.write(data_chunk)
 
 
-def load_iris(file_path: pathlib.Path) -> np.ndarray:
-    """
+def load_iris(file_path: pathlib.Path) -> Tuple[np.ndarray, np.ndarray]:
+    """Load Iris data.
+
+    Args:
+        file_path: Path to file from which data will be loaded.
+
+    Returns:
+        Two numpy arrays one with inputs(x) and one with expected outputs(y).
+        Array shapes are (n, 4) and (n, 3).
 
     """
-    pass
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        x, y = [], []
+        for row in mlp.utils.spinner((row for row in reader if row), message='Loading Iris data: '):
+            x.append([float(number) for number in row[:4]])
+            y.append(NAME2VEC[row[4]])
+        x = np.array(x)
+        y = np.array(y)
+    return x, y
 
 
 def main(args: List[str]) -> None:
-    """
+    """Run example
+
+    Args:
+        args: List of arguments given to script.
 
     """
     args = parse_args(args)
 
+    # Download data
+    # pylint: disable=no-member
     if not IRIS_FILE_PATH.exists():
         download_iris(IRIS_FILE_PATH)
 
-    load_iris()
-    print(f'Loading Iris data from {IRIS_FILE_PATH.absolute()}')
+    # Load data
+    x, y = load_iris(IRIS_FILE_PATH)
+
+    # Split data into train and test set
+    train_x = np.vstack([x[0:40], x[50:90], x[100:140]])
+    train_y = np.vstack([y[0:40], y[50:90], y[100:140]])
+    test_x = np.vstack([x[40:50], x[90:100], x[140:150]])
+    test_y = np.vstack([y[40:50], y[90:100], y[140:150]])
+    print()
+
+    # Load or create estimator
+    if args.input_model_path:
+        estimator = mlp.MultilayerPerceptron.load(args.input_model_path)
+    elif args.output_model_path:
+        estimator = mlp.MultilayerPerceptron(inputs=4, units=[5, 3])
+    print(f'Model summary:\n{estimator}\n')
+
+    # Train estimator if is new
+    if args.output_model_path:
+        print(f'Training:')
+        estimator.train(
+            train_x,
+            train_y,
+            batch_size=1,
+            epochs=10,
+            learning_rate=0.1,
+            momentum=0.7,
+            shuffle=True,
+            metrics_name='categorical_accuracy',
+            early_stopping=True
+        )
+        print()
+
+    # Test estimator
+    train_acc = estimator.evaluate(train_x, train_y, metrics_name='categorical_accuracy')
+    train_mse = estimator.evaluate(train_x, train_y, metrics_name='mean_squared_error')
+    test_acc = estimator.evaluate(test_x, test_y, metrics_name='categorical_accuracy')
+    test_mse = estimator.evaluate(test_x, test_y, metrics_name='mean_squared_error')
+    print(
+        f'Evaluation results:\n'
+        f'  train accuracy: {train_acc:.4f}\n'
+        f'  train mse:      {train_mse:.4f}\n'
+        f'  test accuracy:  {test_acc:.4f}\n'
+        f'  test mse:       {test_mse:.4f}\n'
+    )
+
+    # Save model if is new
+    if args.output_model_path:
+        estimator.save(args.output_model_path)
+        print(f'Saved model to {args.output_model_path.absolute()}')
 
 
 if __name__ == '__main__':
